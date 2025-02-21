504413cedd67edd93b4afcda239c3c25f73f1472
Giulio <giuliopanza2000@gmail.com>
2025-02-21 15:39:53 +0100

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import Cards from "./Card";
+
+const Main = (props) => {
+    
+    return (
+    <main>
+        <div className="container-80"></div>
+    </main>
+)
+    
+}
+
+export default Main;
